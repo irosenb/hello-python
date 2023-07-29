7ce6b2f87ce547ca5429ba1418d32caa618f2ef1
Isaac Rosenberg <irosenb7@gmail.com>
2023-07-29 16:15:08 -0400

    Successfully pulls an Airtable table

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+import os
+from pyairtable import Table
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-db = SQLAlchemy(app)
-
-class Todo(db.Model): 
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self): 
-        return '<Task %r>' % self.id
-
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/events')
+def retrieve_events():
+    events = Table(os.environ['AIRTABLE_API_KEY'], 'appkgFanuGmwzHgbv', 'tblipl9Nu1Q2ylZLs')
+    print(events.all())
+    return "hello world!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
