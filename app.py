7525dd1f107795f81676fcc5b7502adf7b0e2cb2
Isaac Rosenberg <irosenb7@gmail.com>
2023-08-01 13:15:14 -0400

    Events page shows one event

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 import os
@@ -7,18 +6,15 @@ from pyairtable import Table
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return "Hello world"
 
 @app.route('/events')
 def retrieve_events():
     events = Table(os.environ['AIRTABLE_API_KEY'], 'appkgFanuGmwzHgbv', 'tblipl9Nu1Q2ylZLs')
-    print(events.all())
-    return "hello world!"
-
+    return render_template('index.html', events=events.all())
 
 if __name__ == '__main__':
     app.run(debug=True)
